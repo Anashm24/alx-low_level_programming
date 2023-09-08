6303260b127124577ab29266f63f4472d11c5f68
Anashm24 <anashoumaid19@gmail.com>
2023-09-08 20:38:05 +0000

    task is done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-
-/**
- * main -this main function
- *
- * return: always 0
- */
-
-int main(void) {
-	
-	puts("\"Programming is like building a multilingual puzzle": n\);
-
-        return (0);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle" : n\);
+	return (0);
 }
