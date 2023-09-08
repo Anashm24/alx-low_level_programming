137438a6ef2b66a17611000e938dcded53af18f5
Anashm24 <anashoumaid19@gmail.com>
2023-09-08 21:04:21 +0000

    task is done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * description: C program that prints the put function
+ *
+ * Return: always 0
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle" : n\);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
