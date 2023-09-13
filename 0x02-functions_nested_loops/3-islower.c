9c413701d854b407ed944b71d38cef51166750b4
Anashm24 <anashoumaid19@gmail.com>
2023-09-13 18:05:05 +0000

    task is done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - entry point
+ *
+ * Description: check 'c' if it is lowercase or not
+ *
+ * Return: always 0
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	c = _islower('c');
+		return (1);
+	c = _islower('C');
+		return (0);
+}
