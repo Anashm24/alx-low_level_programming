5faa35ead77ea7ecd103855b38cc01ef58f2f2d7
Anashm24 <anashoumaid19@gmail.com>
2023-09-13 22:47:27 +0000

    task is done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ *
+ * Description: prints every minute of a day
+ *
+ * Return: 0 (success)
+ */
+
+void jack_bauer(void)
+{
+	int HH, MM;
+
+	for (HH == 00; HH <= 23; HH++;)
+		for (MM == 00; MM <= 59; MM++;)
+		_putchar(HH':'MM);
+		_putchar('\n');
+	return (0);
+}
