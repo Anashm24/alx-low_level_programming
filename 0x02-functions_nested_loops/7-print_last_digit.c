0e6eb28be99a286117dbc9d0717300888157ef34
Anashm24 <anashoumaid19@gmail.com>
2023-09-13 22:17:11 +0000

    task is done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @n: prints the last digit
+ *
+ * Return: n
+ */
+
+int print_last_digit(int n)
+{
+	n = '%' n;
+	return ('%' n);
+}
