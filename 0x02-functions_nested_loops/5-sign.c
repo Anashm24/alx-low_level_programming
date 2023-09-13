4435264a7674135e70e104e4868a9ab54b90d904
Anashm24 <anashoumaid19@gmail.com>
2023-09-13 21:31:24 +0000

    task is done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *
+ * @n: prints the sign of number
+ *
+ * Return: 1 , 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	if (n == 0)
+		_putchar('0');
+		return (0);
+	if
+		_putchar('_');
+		return (-1);
+}
