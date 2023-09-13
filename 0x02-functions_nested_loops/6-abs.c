b5ef34d72323880706a3d87293314f21d91f303c
Anashm24 <anashoumaid19@gmail.com>
2023-09-13 22:00:33 +0000

    task is done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * @n: computes the absolute value of an int
+ *
+ * Return: n
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+		n = (-1) * n;
+		return (n);
+}
