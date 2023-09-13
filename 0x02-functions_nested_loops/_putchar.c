ee845fb4ff4bca4294d6f7298c6c8700f65d508b
Anashm24 <anashoumaid19@gmail.com>
2023-09-13 14:34:29 +0000

    task is done

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,5 @@
 #include "main.h"
 
-/**
- * main - Entry point
- *
- * Descriton: Write a program that prints _putchar
- *
- * Return: always 0 (success)
- */
-
 int _putchar(char)
 {
 	return (char);
