4a2d6a0463702ab66f9784fdc823c565b46ef0b6
Anashm24 <anashoumaid19@gmail.com>
2023-09-13 23:05:03 +0000

    task is done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,9 +12,16 @@ void jack_bauer(void)
 {
 	int HH, MM;
 
-	for (HH == 00; HH <= 23; HH++)
-		for (MM == 00; MM <= 59; MM++)
-		_putchar(HH':'MM);
-		_putchar('\n');
-	return (0);
+	for (HH == 0; HH <= 23; HH++)
+	{
+		for (MM == 0; MM <= 59; MM++)
+		{
+		_putchar((HH / 10) + '0');
+		_putchar((HH % 10) + '0');
+		_putchar(':');
+		_putchar((MM / 10) + '0');
+		_putchar((MM % 10) + '0');
+		_putchar('\n')
+		}
+	}
 }
