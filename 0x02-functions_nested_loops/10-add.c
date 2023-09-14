69cc43f9b5fdbb2daac433aae82d83590c38436f
Anashm24 <anashoumaid19@gmail.com>
2023-09-14 00:08:38 +0000

    task is done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: sum of a & b
+ */
+
+int add(int a, int b)
+{
+	sum = add(1, 2);
+	return (sum);
+}
