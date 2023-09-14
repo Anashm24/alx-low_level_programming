0c89937d869d11c431f6f003e0d5c887215df621
Anashm24 <anashoumaid19@gmail.com>
2023-09-14 00:27:59 +0000

    task is done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+ * print_to_98 - Entry point
+ *
+ * @n: prints all natural numbers from n to 98
+ *
+ * return: n
+ */
+
+void print_to_98(int n)
+{
+	for (n = 0; n <= 98; n++)
+		_putchar(',');
+		_putchar(' ');
+		_putchar('\n');
+}
