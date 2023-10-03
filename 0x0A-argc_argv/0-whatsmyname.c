580769963b145f9b9638da047802760acda0064b
Anashm24 <anashoumaid19@gmail.com>
2023-10-03 02:07:50 +0000

    task is done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: program that prints its name 
+ *
+ * return: argc and argv
+ */
+
+int main( int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+}
