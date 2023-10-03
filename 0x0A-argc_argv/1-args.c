12290d2932d8c753dbbbc84718aaf372170d31dc
Anashm24 <anashoumaid19@gmail.com>
2023-10-03 02:16:35 +0000

    task is done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: count
+ * @argv: value or parameter
+ *
+ * Return: 0 (succed)
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
