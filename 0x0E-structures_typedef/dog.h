16e1e64c94ce0ccf93c68c04a1bfbf05ed889ae2
Anashm24 <anashoumaid19@gmail.com>
2023-10-10 19:06:04 +0000

    task is done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * initializes a variable of type struct dog
+ *
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ */
+
+struct dog
+{
+	char name,
+	float age,
+	char owner;
+};
+
+#endif
