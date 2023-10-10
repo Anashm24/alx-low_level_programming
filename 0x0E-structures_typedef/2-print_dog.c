5e4697c02e6f04f35df6d8cf5f176ded7c1bb12a
Anashm24 <anashoumaid19@gmail.com>
2023-10-10 23:20:23 +0000

    task is done

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+#include <stdlib>
+
+/**
+ * print_dog - prints a struct dog
+ * @d: the dog to print
+ *
+ * Return: void.
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf("");
+	}
+	if (name == NULL)
+	{
+		printf("Name: (nil)");
+	}
+	if (age == NULL || owner == NULL)
+	{
+		printf("(nil)")
+	}
+	printf("%s\n %lf\n %s", d->name, d->age, d->owner);
+}
