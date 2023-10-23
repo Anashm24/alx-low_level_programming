7eb77cbd474ed482644fd977d9297de748029568
Anashm24 <anashoumaid19@gmail.com>
2023-10-23 18:50:52 +0000

    task is done

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "list.h"
+
+/**
+ * print_listint - singly linked list
+ * @h: a pointer of type listint_t
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++
+	}
+	return (count);
+}
