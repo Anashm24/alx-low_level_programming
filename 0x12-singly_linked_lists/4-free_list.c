513c4fd4d6735812081f1163812bc4da937ac1d8
Anashm24 <anashoumaid19@gmail.com>
2023-10-23 14:37:00 +0000

    task is done

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -9,11 +9,13 @@
  */
 void free_list(list_t *head)
 {
-	if (head)
+	list_t *nextnode;
+
+	while (head != NULL)
 	{
-		free_list(head->next);
-		if (head->str)
-			free(head->str);
+		nextnode = head->next;
+		free(head->str);
 		free(head);
+		head = nextnode;
 	}
 }
