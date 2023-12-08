a2f6850d69b8202dd928633ef66183d1b6b7d1ef
Anashm24 <anashoumaid19@gmail.com>
2023-12-08 18:05:24 +0100

    update

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * dlistint_len - return the number of nodes
- *
- * @h: head of the list
- * Return: the number of nodes
- */
+* dlistint_len - return the number of nodes
+*
+* @h: head of the list
+* Return: the number of nodes
+*/
 size_t dlistint_len(const dlistint_t *h)
 {
 	int count;
@@ -20,7 +20,7 @@ size_t dlistint_len(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-        h = h->next;
+		h = h->next;
 		count++;
 	}
 
