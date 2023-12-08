8957608199ed32a623ce019ddd036c6e71e009b5
Anashm24 <anashoumaid19@gmail.com>
2023-12-08 17:34:38 +0100

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,6 +11,13 @@ size_t print_dlistint(const dlistint_t *h)
 	dlistint_t *current = h;
 	int count = 0;
 
+	if (current == NULL)
+		return (count);
+
+	while (current->prev != NULL)
+		current = current->prev;
+
+
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
