d90c6770745c0de35e54eb167dd3918353a7bfa6
Anashm24 <anashoumaid19@gmail.com>
2023-12-22 20:18:24 +0100

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,17 +10,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
-	unsigned long int i;
 
-	if (table == NULL)
-		return (NULL);
-	table->size = size;
-	table->array = malloc(sizeof(hash_node_t) * size);
-
-	if (table->array == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		table->array[i] = NULL;
-
+	if (table != NULL)
+	{
+		table->size = size;
+		table->array = calloc(table->size, sizeof(hash_node_t *));
+		if (table->array == NULL)
+		{
+			free(table);
+			return (NULL);
+		}
+	}
 	return (table);
 }
